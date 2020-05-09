363420e41e4ec78f1517e1e6d1d8d4d09fd86165
Parag Bhosle <paragbhosle201088@gmail.com>
2020-05-09 15:26:59 +0530

    Second Commit

---
+++ Assignment1/Assignment1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+namespace BasicThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Thread T1 = new Thread(ThreadStartFunction);
+            T1.Start();
+            Thread.Sleep(2000);
+            Console.WriteLine("I have Started the Thread");
+        }
+
+        static void ThreadStartFunction()
+        {
+
+            for (var local = 0; local < 10; local++)
+            {
+                Console.WriteLine($"Local={local}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
