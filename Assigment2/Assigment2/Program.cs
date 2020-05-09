80a90ee368dffb508acbbb9e45b569ba5028e0de
Parag Bhosle <paragbhosle201088@gmail.com>
2020-05-09 15:22:05 +0530

    First Commit

---
+++ Assigment2/Assigment2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+namespace Assigment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread Eena = new Thread(ThreadStartFunction);
+            Eena.Name = "Eena";
+            Thread Meena = new Thread(ThreadStartFunction);
+            Meena.Name = "Meena";
+            Thread Dika = new Thread(ThreadStartFunction);
+            Dika.Name = "Dika";
+            Eena.Start();
+            Meena.Start();
+            Dika.Start();
+            Console.WriteLine("Done!");
+        }
+
+        static void ThreadStartFunction() {
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"For Thread :{Thread.CurrentThread.Name} and i={i}");
+                Thread.Sleep(1000);
+            }
+        } 
+    }
+}
