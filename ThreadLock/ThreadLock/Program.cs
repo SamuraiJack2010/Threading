1dd759f486720e03ee2539d9cc12570c020792fe
Parag Bhosle <paragbhosle201088@gmail.com>
2020-05-09 19:36:36 +0530

    3rd Commit

---
+++ ThreadLock/ThreadLock/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadLock
+{
+    class Program
+    {
+        //Single object MyLock Used with lock.
+        //This .net object is used for Achieving the encapsulation of a critical section.
+        //lock is nothing but Monitor object with Enter and Exit function.
+        static object myObject = new object();
+        static void Main(string[] args)
+        {
+            Thread ping = new Thread(MyThread);
+            Thread pong = new Thread(MyThread);
+            ping.Name = "ping";
+            pong.Name = "pong";
+            ping.Start();
+            pong.Start();
+        }
+
+        static void MyThread() {
+            //Critical Section has been defended via lock using myObject.
+            //myObject plays an critical part in encapsulation.i.e. at a given time only one thread can access 
+            //the code within lock(){}
+            //If we use new object() instead of myObject,then all the threads will access the code within lock(){}
+            //simultaneously
+            try
+            {
+                lock (myObject)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.Name} started");
+                    Thread.Sleep(500);
+                    //throw new Exception("Test"); //Check how lock is handled when error occurs
+                    Console.WriteLine($"{Thread.CurrentThread.Name} stopped");
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ ThreadMonitor/ThreadMonitor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadMonitor
+{
+    class Program
+    {
+        //Single object MyLock Used for Monitor.
+        //This .net object is used for Achieving the encapsulation of a critical section.
+        //Used in Monitor Enter and Exit function.
+        
+        static object myLock = new object();
+        static void Main(string[] args)
+        {
+            Thread ping = new Thread(TakeTurns);
+            Thread pong = new Thread(TakeTurns);
+            ping.Name = "ping";
+            pong.Name = "pong";
+            ping.Start();
+            pong.Start();
+        }
+        static void TakeTurns() {
+            //If we use new object() instead of myLock Below. Then lock will not be achieved 
+            //and threads ping and pong will access the function simultaneously.
+            Monitor.Enter(myLock);
+            try
+            {
+                Console.WriteLine($"{ Thread.CurrentThread.Name } started");
+                Thread.Sleep(2000);
+                Console.WriteLine($"{ Thread.CurrentThread.Name } stopped");
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Monitor.Exit(myLock);
+                Console.WriteLine("Inside Finally");
+            }
+            Console.WriteLine("OutSide Finally");
+
+        }
+    }
+}
